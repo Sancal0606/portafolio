eb519b158a843ac9d91093a1fec19291a077ed17
Sancal0606 <carloz1.2.sanchez@gmail.com>
2024-10-07 16:12:46 +0200

    Project setup and header

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: "/portafolio",
   plugins: [react()],
+  css:{
+    modules: {
+      localsConvention: "camelCase",
+    }
+  }
 })
